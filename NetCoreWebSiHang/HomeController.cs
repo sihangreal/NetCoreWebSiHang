68915e9834b4a22fe3f69ad7dd432f068b622fa9
sihangreal <137238384@qq.com>
2019-06-22 23:26:31 +0800

    first commit

---
+++ NetCoreWebSiHang/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NetCoreWebSiHang
+{
+    [Route("home")]
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("hello")]
+        public string hello()
+        {
+            return "hello lulu";
+        }
+    }
+}
