6897fb623be8490ed9c0d106e480feb2ea154cbc
chensihang <137238384@qq.com>
2019-06-24 22:52:54 +0800

    启动

--- NetCoreWebSiHang/Program.cs
+++ NetCoreWebSiHang/Program.cs
@@ -14,15 +14,16 @@ namespace NetCoreWebSiHang
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-             .UseKestrel()
-             .UseContentRoot(Directory.GetCurrentDirectory())
-             .UseIISIntegration()
-             .UseUrls("http://*:9100")//监听来自所有ip的9100端口的请求
-             .UseStartup<Startup>()
-             .Build();
+            //var host = new WebHostBuilder()
+            // .UseKestrel()
+            // .UseContentRoot(Directory.GetCurrentDirectory())
+            // .UseIISIntegration()
+            // .UseUrls("http://*:9100")//监听来自所有ip的9100端口的请求
+            // .UseStartup<Startup>()
+            // .Build();
 
-            host.Run();
+            //host.Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
